c100ec7a6339f965fe8a9fb078290c2245bee9ac
switbartlomiej@gmail.com <switbartlomiej@gmail.com>
2024-03-05 01:57:03 +0100

    Pong_wall

--- pong/pong.js
+++ pong/pong.js
@@ -5,18 +5,42 @@ function runPongScript() {
     const canvWidth = canvas.width;
     const canvHeight = canvas.height;
 
+    const linaWidth = 5;
+    const lineHeight = 10;
+
     function drawBackground() {
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, canvWidth, canvHeight);
+
+        ctx.fillStyle = 'white';
+        //ctx.fillRect(canvWidth / 2, 0, 2, canvHeight);
+        for (let i = 5; i < canvHeight; i += 20) {
+            ctx.fillRect(canvWidth / 2 - linaWidth / 2, i, linaWidth, lineHeight);
+        }
+
     }
 
-    const bollSize = 20;
-    let bollX = canvWidth / 2 - bollSize / 2;
-    let bollY = canvHeight / 2 - bollSize / 2;
+    const ballSize = 20;
+    let ballX = canvWidth / 2 - ballSize / 2;
+    let ballY = canvHeight / 2 - ballSize / 2;
 
-    function drawBoll() {
+    let ballSpeedX = 1;
+    let ballSpeedY = 1;
+
+    function drawBall() {
         ctx.fillStyle = 'red';
-        ctx.fillRect(bollX, bollY, bollSize, bollSize);
+        ctx.fillRect(ballX, ballY, ballSize, ballSize);
+
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        if (ballY < 0 + ballSize) {
+            ballSpeedY = -ballSpeedY;
+        }
+
+        if (ballY > canvHeight - ballSize) {
+            ballSpeedY = -ballSpeedY;
+        }
     }
 
     const paddleHeight = 100;
@@ -28,10 +52,52 @@ function runPongScript() {
     let leftPaddleY = canvHeight / 2 - paddleHeight / 2;
     let rightPaddleY = canvHeight / 2 - paddleHeight / 2;
 
+    function drawPaddle(x, y) {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(x, y, paddleWidth, paddleHeight);
+    }
+
+    function gameRuning() {
+        //drawEverything();
+        //moveEverything();
+        drawBackground();
+        drawBall();
+        drawPaddle(leftPaddleX, leftPaddleY);
+        drawPaddle(rightPaddleX, rightPaddleY);
+    }
+
+    function moveEverything() {
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        if (ballX < 0) {
+            if (ballY > leftPaddleY && ballY < leftPaddleY + paddleHeight) {
+                ballSpeedX = -ballSpeedX;
+            } else {
+                ballReset();
+            }
+        }
+
+        if (ballX > canvWidth) {
+            if (ballY > rightPaddleY && ballY < rightPaddleY + paddleHeight) {
+                ballSpeedX = -ballSpeedX;
+            } else {
+                ballReset();
+            }
+        }
+
+        if (ballY < 0) {
+            ballSpeedY = -ballSpeedY;
+        }
+
+        if (ballY > canvHeight) {
+            ballSpeedY = -ballSpeedY;
+        }
+    }
 
 
-    drawBackground();
-    drawBoll();
+    setInterval(gameRuning, 1000 / 60);
+
 
 
 
