f7d3d1565818c85ecf26c2a2e929a3e9cddbe525
switbartlomiej@gmail.com <switbartlomiej@gmail.com>
2024-03-07 01:39:17 +0100

    Pong_finish_basic_logic

--- pong/pong.js
+++ pong/pong.js
@@ -24,28 +24,27 @@ function runPongScript() {
     let ballX = canvWidth / 2 - ballSize / 2;
     let ballY = canvHeight / 2 - ballSize / 2;
 
-    let ballSpeedX = 3;
-    let ballSpeedY = 3;
+    let ballSpeedX = 1;
+    let ballSpeedY = 1;
+
+    let blink = false;
 
     function drawBall(x, y) {
         //ctx.fillStyle = 'yellow';
         //ctx.fillRect(x, y, ballSize, ballSize);
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = (blink) ? 'red' : 'white';
         ctx.beginPath(x, y);
-        ctx.arc(x + 10, y + 10, 10, 0, 2 * Math.PI);
+        ctx.arc(x + 10, y + 10, ballSize / 2, 0, 2 * Math.PI);
         ctx.fill();
+    }
 
-        ballX += ballSpeedX;
-        ballY += ballSpeedY;
+    function drawPaddle(x, y) {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(x, y, paddleWidth, paddleHeight);
+    }
 
-        if (ballY <= 0  || ballY >= canvHeight - ballSize) {
-            ballSpeedY = -ballSpeedY;
-        }
-
-        if (ballX <= 0 || ballX >= canvWidth - ballSize) {
-            ballSpeedX = -ballSpeedX;
-        }
-
+    function blinkBall() {
+        blink = !blink;
     }
 
     const paddleHeight = 100;
@@ -57,18 +56,41 @@ function runPongScript() {
     let leftPaddleY = canvHeight / 2 - paddleHeight / 2;
     let rightPaddleY = canvHeight / 2 - paddleHeight / 2;
 
-    function drawPaddle(x, y) {
-        ctx.fillStyle = 'white';
-        ctx.fillRect(x, y, paddleWidth, paddleHeight);
-    }
 
-    function gameRuning() {
-        //drawEverything();
-        //moveEverything();
-        drawBackground();
-        drawBall(ballX, ballY);
-        drawPaddle(leftPaddleX, leftPaddleY);
-        drawPaddle(rightPaddleX, rightPaddleY);
+    function moveBall() {
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        if (ballY <= 0  || ballY >= canvHeight - ballSize) {
+            ballSpeedY = -ballSpeedY;
+            ballSpeedUp();
+        }
+
+        /*if (ballX <= 0 || ballX >= canvWidth - ballSize) {
+            ballSpeedX = -ballSpeedX;
+            ballSpeedUp();
+        }*/
+
+        if (ballX <= leftPaddleX + paddleWidth && ballY >= leftPaddleY && ballY <= leftPaddleY + paddleHeight) {
+            ballSpeedX = -ballSpeedX;
+            ballSpeedUp();
+            blinkBall();
+        } else if (ballX <= 0) {
+            ballReset();
+            ballSpeedX = -1;
+            ballSpeedY = -1;
+        }
+
+        if (ballX >= rightPaddleX - paddleWidth && ballY >= rightPaddleY && ballY <= rightPaddleY + paddleHeight) {
+            ballSpeedX = -ballSpeedX;
+            ballSpeedUp();
+            blinkBall();
+        } else if (ballX >= canvWidth - ballSize) {
+            ballReset();
+            ballSpeedX = 1;
+            ballSpeedY = 1;
+        }
+
     }
 
     /*window.addEventListener("click", 
@@ -76,10 +98,25 @@ function runPongScript() {
         console.log("click");
     });*/
 
+    function ballSpeedUp() {
+        if (ballSpeedX > 0 && ballSpeedX < 16) {
+            ballSpeedX += 0.5;
+        } else if (ballSpeedX < 0 && ballSpeedX > -16) {
+            ballSpeedX -= 0.5;
+        }
+
+        if (ballSpeedY > 0 && ballSpeedY < 16) {
+            ballSpeedY += 0.5;
+        } else if (ballSpeedY < 0 && ballSpeedY > -16) {
+            ballSpeedY -= 0.5;
+        }
+    }
+
     function ballReset() {
         ballX = canvWidth / 2 - ballSize / 2;
         ballY = canvHeight / 2 - ballSize / 2;
     }
+
     canvasPosition = canvas.getBoundingClientRect();
     console.log(canvasPosition);
 
@@ -94,47 +131,62 @@ function runPongScript() {
         if (leftPaddleY <= 0) {
             leftPaddleY = 0;
         }
+        //rightPaddleY = leftPaddleY;
     }
 
+    aiCalculate = function () {
+        const rightPaddleYCenter = rightPaddleY + paddleHeight / 2;
+        const ballYCenter = ballY + ballSize / 2;
 
+        if (rightPaddleYCenter < ballY - 35) {
+            rightPaddleY += 9;
+        } else if (rightPaddleYCenter > ballY + 35) {
+            rightPaddleY -= 9;
+        }
+        
+        /*
+        if (ballX > canvWidth / 2) {
+            if (rightPaddleYCenter - ballYCenter > 200) {
+                rightPaddleY -= 2;
+            } else if (rightPaddleYCenter - ballYCenter > 50) {
+                rightPaddleY -= 5;
+            } else if (rightPaddleYCenter - ballYCenter < -200) {
+                rightPaddleY += 20;
+            } else if (rightPaddleYCenter - ballYCenter < -50) {
+                rightPaddleY += 5;
+            }       
+
+        }if (ballX <= canvWidth / 2 && ballX > 200) {
+            if (rightPaddleYCenter - ballYCenter > 100) {
+                rightPaddleY -= 10;
+            } else if (rightPaddleYCenter - ballYCenter < -100) {
+                rightPaddleY += 10;
+            }
+        }
+        */        
+    }
+
+    //To do smart AI for right paddle
+    //To do add arrow keys for right and left paddle
+    //To do add score
+    //To do add two players mode
+    //To do add sound
+    //To do add game over
+    //To do add start game
+    //To do add pause game
+    //To do add restart game
+    //To do change speed ball change !!!!!!!!!
+
+    function gameRuning() {
+        moveBall();
+        aiCalculate();
+        drawBackground();
+        drawBall(ballX, ballY);
+        drawPaddle(leftPaddleX, leftPaddleY);
+        drawPaddle(rightPaddleX, rightPaddleY);        
+    }
 
     canvas.addEventListener("mousemove", leftPaddlePosition);
-       
- 
-
-    function moveEverything() {
-        ballX += ballSpeedX;
-        ballY += ballSpeedY;
-
-        if (ballX < 0) {
-            if (ballY > leftPaddleY && ballY < leftPaddleY + paddleHeight) {
-                ballSpeedX = -ballSpeedX;
-            } else {
-                ballReset();
-            }
-        }
-
-        if (ballX > canvWidth) {
-            if (ballY > rightPaddleY && ballY < rightPaddleY + paddleHeight) {
-                ballSpeedX = -ballSpeedX;
-            } else {
-                ballReset();
-            }
-        }
-
-        if (ballY < 0) {
-            ballSpeedY = -ballSpeedY;
-        }
-
-        if (ballY > canvHeight) {
-            ballSpeedY = -ballSpeedY;
-        }
-    }
-
-
     setInterval(gameRuning, 1000 / 60);
-
-
-
 
 }
